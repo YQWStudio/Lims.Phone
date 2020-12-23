84400f4a669a4a13ad0227722b83c85287021c3c
尹庆文 <Yinqingwen@gmail.com>
2020-12-23 11:46:05 +0800

    调整校验模式，付款方式及中转方式不强制校验，电话不校验格式，收货地址不校验

--- Lims.Phone/ViewModels/ShippingViewModelValidator.cs
+++ Lims.Phone/ViewModels/ShippingViewModelValidator.cs
@@ -12,8 +12,8 @@ namespace Lims.Phone.ViewModels
             //收货人电话不能为空或不符合格式
             RuleFor(item => item.ConsigneePhone)
                 .NotNull().WithMessage("收货人电话不能为空，请检查！！！")
-                .NotEmpty().WithMessage("收货人电话不能为空，请检查！！！")
-                .Matches("^1[345789]\\d{9}$|^0[0-9]{2,3}-[0-9]{7,8}").WithMessage("电话号码格式错误，请检查！！！");
+                .NotEmpty().WithMessage("收货人电话不能为空，请检查！！！");
+                //.Matches("^1[345789]\\d{9}$|^0[0-9]{2,3}-[0-9]{7,8}").WithMessage("电话号码格式错误，请检查！！！");
 
             //收货人姓名不能为空
             RuleFor(item => item.ConsigneeName)
@@ -21,15 +21,15 @@ namespace Lims.Phone.ViewModels
                 .NotEmpty().WithMessage("收货人姓名不能为空，请检查！！！");
 
             //收货人地址为空检查
-            RuleFor(item => item.ConsigneeAddress)
+            /*RuleFor(item => item.ConsigneeAddress)
                 .NotNull().WithMessage("收货人地址不能为空，请检查！！！")
                 .NotEmpty().WithMessage("收货人地址不能为空，请检查！！！");
-
+            */
             //发货人电话为空和格式检查
             RuleFor(item => item.ShipperPhone)
                 .NotNull().WithMessage("发货人电话不能为空，请检查！！！")
-                .NotEmpty().WithMessage("发货人电话不能为空，请检查！！！")
-                .Matches("^1[345789]\\d{9}$|^0[0-9]{2,3}-[0-9]{7,8}").WithMessage("发货人电话号码格式不符，请检查！！！");
+                .NotEmpty().WithMessage("发货人电话不能为空，请检查！！！");
+                //.Matches("^1[345789]\\d{9}$|^0[0-9]{2,3}-[0-9]{7,8}").WithMessage("发货人电话号码格式不符，请检查！！！");
 
             //发货人姓名为空检查
             RuleFor(item => item.ShipperName)
@@ -62,6 +62,7 @@ namespace Lims.Phone.ViewModels
                 .NotEmpty().WithMessage("运费不能为零，请检查！！！");
 
             //付款方式检查
+            /*
             RuleFor(item => item.PaymentMethods)
                 .NotNull().WithMessage("付款方式不能为空，请检查")
                 .NotEmpty().WithMessage("付款方式不能为空，请检查");
@@ -69,6 +70,7 @@ namespace Lims.Phone.ViewModels
             RuleFor(item => item.TransitMethod)
                 .NotNull().WithMessage("中转方式不能为空，请检查！！！")
                 .NotEmpty().WithMessage("中转方式不能为空，请检查！！！");
+            */
         }
     }
 }
